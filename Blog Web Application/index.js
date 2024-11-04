a9413bfc459375ebf44af4fe077d5c23660247d7
fevnax <haru54709@gmail.com>
2024-11-04 22:33:32 +0530

    Add new features

--- Blog Web Application/index.js
+++ Blog Web Application/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 5500;
+
+// Blog Array
+let blogs = [];
+let blogCounter = 1;
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,10 +15,62 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");;
+    res.render("home.ejs");
+});
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
+app.post("/create", (req, res) => {
+    const blogTitle = req.body['form-title'];
+    const blogDescription = req.body['form-desc'];
+    const blogDetails = { id: blogCounter++, title: blogTitle, description: blogDescription };
+    blogs.push(blogDetails);
+    res.redirect('/blogs');
+});
+
+app.get("/blogs", (req, res) => {
+    res.render("blogs.ejs", { blogs: blogs });
+});
+
+app.get("/blogs/:id/edit", (req, res) => {
+    const blogId = parseInt(req.params.id);
+    const blog = blogs.find(b => b.id === blogId);
+
+    if (blog) {
+        res.render("edit.ejs", { blog: blog });
+    } else {
+        res.status(404).send("Blog not found");
+    }
+});
+
+app.post("/blogs/:id/edit", (req, res) => {
+    const blogId = parseInt(req.params.id);
+    const blogIndex = blogs.findIndex(b => b.id === blogId);
+
+    if (blogIndex !== -1) {
+        blogs[blogIndex].title = req.body['form-title'];
+        blogs[blogIndex].description = req.body['form-desc'];
+        res.redirect('/blogs');
+    } else {
+        res.status(404).send("Blog not found");
+    }
+});
+
+app.post("/blogs/:id/delete", (req, res) => {
+    const blogId = parseInt(req.params.id);
+    const blogIndex = blogs.findIndex(b => b.id === blogId);
+
+    if (blogIndex !== -1) {
+        blogs.splice(blogIndex, 1);
+        res.redirect('/blogs');
+    } else {
+        res.status(404).send("Blog not found");
+    }
 });
 
 // PORT
 app.listen(port, () => {
-    console.log(`Running on port ${port}.`);
+    console.log(`Server running on port ${port}.`);
 });
